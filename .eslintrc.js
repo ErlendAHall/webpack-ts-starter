2393ba4867e7491be73c259d771808053f2a7582
Torbjørn Høiland <t_hoiland@hotmail.com>
2020-05-08 16:02:29 +0200

    added eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,27 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        "ecmaFeatures": {
-            "jsx": true
+        ecmaFeatures: {
+            jsx: true,
         },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+        ecmaVersion: 2018,
+        sourceType: "module",
     },
-    extends: [
-        "plugin:react/recommended",
-        "plugin:prettier/recommended"
-    ],
-    plugins: [
-        "react",
-        "prettier"
-    ],
+    extends: ["plugin:react/recommended", "plugin:prettier/recommended"],
+    plugins: ["react", "prettier"],
     globals: {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
     },
     settings: {
         react: {
-            version: "detect"
+            version: "detect",
         },
     },
     env: {
-        "browser": true,
-        "es6": true
+        browser: true,
+        es6: true,
     },
-    rules: {
-    },
-    overrides: []
+    rules: {},
+    overrides: [],
 };
--- src/ExampleApp/example.tsx
+++ src/ExampleApp/example.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import { wrapper } from "./example.less";
 
 export const Example: React.FC = () => {
-  
   return (
     <main>
-    <div className={wrapper}>CSS Modules</div>
+      <div className={wrapper}>CSS Modules</div>
       <div> API Url: {process.env.API_URL}</div>
     </main>
   );
